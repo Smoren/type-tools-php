f2c3e4e6a52f2bf2b36467089c58bef6d28eaab7
Smoren <ofigate@gmail.com>
2023-01-11 10:24:30 +0300

    `ObjectAccess::hasReadableProperty()`: test added

---
+++ tests/unit/ObjectAccess/HasReadablePropertyValueTest.php
@@ -0,0 +1,138 @@
+<?php
+
+namespace Smoren\TypeTools\Tests\Unit\ObjectAccess;
+
+use Codeception\Test\Unit;
+use Error;
+use Smoren\TypeTools\ObjectAccess;
+use Smoren\TypeTools\Tests\Unit\Fixtures\ClassWithAccessibleProperties;
+use stdClass;
+
+class HasReadablePropertyValueTest extends Unit
+{
+    /**
+     * @param stdClass $input
+     * @param string $key
+     * @return void
+     * @dataProvider fromStdClassTrueDataProvider
+     */
+    public function testFromStdClassTrue(stdClass $input, string $key): void
+    {
+        // When
+        $result = ObjectAccess::hasReadableProperty($input, $key);
+
+        // Then
+        $this->assertTrue($result);
+    }
+
+    public function fromStdClassTrueDataProvider(): array
+    {
+        $wrap = static function(array $input): object {
+            return (object)$input;
+        };
+
+        return [
+            [$wrap(['a' => 1, 'b' => 2]), 'a'],
+            [$wrap(['a' => 1, 'b' => 2]), 'b'],
+        ];
+    }
+
+    /**
+     * @param stdClass $input
+     * @param string $key
+     * @return void
+     * @dataProvider fromStdClassFalseDataProvider
+     */
+    public function testFromStdClassFalse(stdClass $input, string $key): void
+    {
+        // When
+        $result = ObjectAccess::hasReadableProperty($input, $key);
+
+        // Then
+        $this->assertFalse($result);
+    }
+
+    public function fromStdClassFalseDataProvider(): array
+    {
+        $wrap = static function(array $input): object {
+            return (object)$input;
+        };
+
+        return [
+            [$wrap([]), ''],
+            [$wrap([]), ''],
+            [$wrap([]), '0'],
+            [$wrap([]), '0'],
+            [$wrap([]), 'a'],
+            [$wrap([]), 'b'],
+            [$wrap(['a' => 1, 'b' => 2]), ''],
+            [$wrap(['a' => 1, 'b' => 2]), ''],
+            [$wrap(['a' => 1, 'b' => 2]), '0'],
+            [$wrap(['a' => 1, 'b' => 2]), '0'],
+            [$wrap(['a' => 1, 'b' => 2]), '1'],
+            [$wrap(['a' => 1, 'b' => 2]), '1'],
+            [$wrap(['a' => 1, 'b' => 2]), '2'],
+            [$wrap(['a' => 1, 'b' => 2]), '2'],
+        ];
+    }
+
+    /**
+     * @param object $input
+     * @param string $key
+     * @return void
+     * @dataProvider fromObjectTrueDataProvider
+     */
+    public function testFromObjectTrue(object $input, string $key): void
+    {
+        // When
+        $result = ObjectAccess::hasReadableProperty($input, $key);
+
+        // Then
+        $this->assertTrue($result);
+    }
+
+    public function fromObjectTrueDataProvider(): array
+    {
+        return [
+            [new ClassWithAccessibleProperties(), 'publicProperty'],
+            [new ClassWithAccessibleProperties(), 'publicProperty'],
+            [new ClassWithAccessibleProperties(), 'publicPropertyWithGetterAccess'],
+            [new ClassWithAccessibleProperties(), 'publicPropertyWithGetterAccess'],
+            [new ClassWithAccessibleProperties(), 'protectedPropertyWithGetterAccess'],
+            [new ClassWithAccessibleProperties(), 'protectedPropertyWithGetterAccess'],
+            [new ClassWithAccessibleProperties(), 'privatePropertyWithGetterAccess'],
+            [new ClassWithAccessibleProperties(), 'privatePropertyWithGetterAccess'],
+        ];
+    }
+
+    /**
+     * @param object $input
+     * @param string $key
+     * @return void
+     * @dataProvider fromObjectFalseDataProvider
+     */
+    public function testFromObjectFalse(object $input, string $key): void
+    {
+        // When
+        $result = ObjectAccess::hasReadableProperty($input, $key);
+
+        // Then
+        $this->assertFalse($result);
+    }
+
+    public function fromObjectFalseDataProvider(): array
+    {
+        return [
+            [new ClassWithAccessibleProperties(), ''],
+            [new ClassWithAccessibleProperties(), ''],
+            [new ClassWithAccessibleProperties(), '0'],
+            [new ClassWithAccessibleProperties(), '0'],
+            [new ClassWithAccessibleProperties(), 'unknownProperty'],
+            [new ClassWithAccessibleProperties(), 'unknownProperty'],
+            [new ClassWithAccessibleProperties(), 'protectedProperty'],
+            [new ClassWithAccessibleProperties(), 'protectedProperty'],
+            [new ClassWithAccessibleProperties(), 'privateProperty'],
+            [new ClassWithAccessibleProperties(), 'privateProperty'],
+        ];
+    }
+}
