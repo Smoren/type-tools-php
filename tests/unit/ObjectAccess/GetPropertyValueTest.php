db2ddae69991d09bc0d9a9c6d93b21270bc2bb9a
Smoren <ofigate@gmail.com>
2023-01-11 10:16:17 +0300

    `ObjectAccess::getPropertyValue()` test added.

---
+++ tests/unit/ObjectAccess/GetPropertyValueTest.php
@@ -0,0 +1,144 @@
+<?php
+
+namespace Smoren\TypeTools\Tests\Unit\ObjectAccess;
+
+use Codeception\Test\Unit;
+use Error;
+use Smoren\TypeTools\ObjectAccess;
+use Smoren\TypeTools\Tests\Unit\Fixtures\ClassWithAccessibleProperties;
+use stdClass;
+
+class GetPropertyValueTest extends Unit
+{
+    /**
+     * @param stdClass $input
+     * @param string $key
+     * @param mixed $expected
+     * @return void
+     * @dataProvider fromStdClassSuccessDataProvider
+     */
+    public function testFromStdClassSuccess(stdClass $input, string $key, $expected): void
+    {
+        // When
+        $result = ObjectAccess::getPropertyValue($input, $key);
+
+        // Then
+        $this->assertEquals($expected, $result);
+    }
+
+    public function fromStdClassSuccessDataProvider(): array
+    {
+        $wrap = static function(array $input): object {
+            return (object)$input;
+        };
+
+        return [
+            [$wrap(['a' => 1, 'b' => 2]), 'a', 1],
+            [$wrap(['a' => 1, 'b' => 2]), 'b', 2],
+        ];
+    }
+
+    /**
+     * @param stdClass $input
+     * @param string $key
+     * @return void
+     * @dataProvider fromStdClassFailDataProvider
+     */
+    public function testFromStdClassFail(stdClass $input, string $key): void
+    {
+        try {
+            // When
+            ObjectAccess::getPropertyValue($input, $key);
+            $this->fail();
+        } catch(Error $e) {
+            // Then
+        }
+    }
+
+    public function fromStdClassFailDataProvider(): array
+    {
+        $wrap = static function(array $input): object {
+            return (object)$input;
+        };
+
+        return [
+            [$wrap([]), ''],
+            [$wrap([]), ''],
+            [$wrap([]), '0'],
+            [$wrap([]), '0'],
+            [$wrap([]), 'a'],
+            [$wrap([]), 'b'],
+            [$wrap(['a' => 1, 'b' => 2]), ''],
+            [$wrap(['a' => 1, 'b' => 2]), ''],
+            [$wrap(['a' => 1, 'b' => 2]), '0'],
+            [$wrap(['a' => 1, 'b' => 2]), '0'],
+            [$wrap(['a' => 1, 'b' => 2]), '1'],
+            [$wrap(['a' => 1, 'b' => 2]), '1'],
+            [$wrap(['a' => 1, 'b' => 2]), '2'],
+            [$wrap(['a' => 1, 'b' => 2]), '2'],
+        ];
+    }
+
+    /**
+     * @param object $input
+     * @param string $key
+     * @param mixed $expected
+     * @return void
+     * @dataProvider fromObjectSuccessDataProvider
+     */
+    public function testFromObjectSuccess(object $input, string $key, $expected): void
+    {
+        // When
+        $result = ObjectAccess::getPropertyValue($input, $key);
+
+        // Then
+        $this->assertEquals($expected, $result);
+    }
+
+    public function fromObjectSuccessDataProvider(): array
+    {
+        return [
+            [new ClassWithAccessibleProperties(), 'publicProperty', 1],
+            [new ClassWithAccessibleProperties(), 'publicProperty', 1],
+            [new ClassWithAccessibleProperties(), 'publicPropertyWithGetterAccess', 2],
+            [new ClassWithAccessibleProperties(), 'publicPropertyWithGetterAccess', 2],
+            [new ClassWithAccessibleProperties(), 'protectedPropertyWithGetterAccess', 4],
+            [new ClassWithAccessibleProperties(), 'protectedPropertyWithGetterAccess', 4],
+            [new ClassWithAccessibleProperties(), 'privatePropertyWithGetterAccess', 6],
+            [new ClassWithAccessibleProperties(), 'privatePropertyWithGetterAccess', 6],
+        ];
+    }
+
+    /**
+     * @param object $input
+     * @param string $key
+     * @return void
+     * @dataProvider fromObjectFailDataProvider
+     */
+    public function testFromObjectFail(object $input, string $key): void
+    {
+        try {
+            // When
+            ObjectAccess::getPropertyValue($input, $key);
+            $this->fail();
+        } catch(Error $e) {
+            // Then
+        }
+    }
+
+    public function fromObjectFailDataProvider(): array
+    {
+        return [
+            [new ClassWithAccessibleProperties(), ''],
+            [new ClassWithAccessibleProperties(), ''],
+            [new ClassWithAccessibleProperties(), '0'],
+            [new ClassWithAccessibleProperties(), '0'],
+            [new ClassWithAccessibleProperties(), 'unknownProperty'],
+            [new ClassWithAccessibleProperties(), 'unknownProperty'],
+            [new ClassWithAccessibleProperties(), 'protectedProperty'],
+            [new ClassWithAccessibleProperties(), 'protectedProperty'],
+            [new ClassWithAccessibleProperties(), 'privateProperty'],
+            [new ClassWithAccessibleProperties(), 'privateProperty'],
+        ];
+    }
+}
